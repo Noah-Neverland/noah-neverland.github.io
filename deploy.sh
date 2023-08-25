e4c599b9d20eae7056a9aa828f3e03ed0ca95bc8
shuaishuai.han <shuaishuai.han@caetar.com>
2023-08-25 16:17:41 +0800

    docs: 更新github actions文档

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f https://github.com/hss-zq/hss.github.io.git master
+git push -f https://github.com/Noah-Neverland/noah-neverland.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
